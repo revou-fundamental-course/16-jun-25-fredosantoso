1ba0154247863e1b9970463ef68fa7ac625cc705
Fredo Santoso <frodofransiskus@gmail.com>
2025-06-25 23:00:40 +0700

    update js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function calcLuas() {
+    const alas = document.getElementById("alas").value;
+    const tinggi = document.getElementById("tinggi").value;
+
+    if (alas != "" && tinggi != "") {
+        
+    } else {
+        alert("Please fill in both fields.");
+    }
+}
